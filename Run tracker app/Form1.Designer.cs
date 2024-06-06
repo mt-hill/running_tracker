d0ec60af3399ac04d6b840cd466a7b8d9e99cc43
mt-hill <138307546+mt-hill@users.noreply.github.com>
2024-06-06 16:35:36 +0100

    first

---
+++ Run tracker app/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Run_tracker_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.listView = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblBestPace = new System.Windows.Forms.Label();
+            this.lblTotalAvgPace = new System.Windows.Forms.Label();
+            this.lblTotalRuns = new System.Windows.Forms.Label();
+            this.lblTotalDistance = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 22.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(176, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Run Tracker";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTime);
+            this.groupBox1.Controls.Add(this.txtDistance);
+            this.groupBox1.Controls.Add(this.datePicker);
+            this.groupBox1.Font = new System.Drawing.Font("Leelawadee UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(480, 219);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter your latest run:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnSubmit.Font = new System.Drawing.Font("Leelawadee UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(183, 172);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(121, 29);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Time (MM:SS)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Distance (KM)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(183, 128);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(200, 29);
+            this.txtTime.TabIndex = 2;
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDistance.Location = new System.Drawing.Point(183, 93);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(200, 29);
+            this.txtDistance.TabIndex = 1;
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(183, 58);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 29);
+            this.datePicker.TabIndex = 0;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(16, 644);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(480, 178);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblBestPace);
+            this.groupBox2.Controls.Add(this.lblTotalAvgPace);
+            this.groupBox2.Controls.Add(this.lblTotalRuns);
+            this.groupBox2.Controls.Add(this.lblTotalDistance);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("Leelawadee UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(18, 299);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 339);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Overall Stats";
+            // 
+            // lblBestPace
+            // 
+            this.lblBestPace.AutoSize = true;
+            this.lblBestPace.Font = new System.Drawing.Font("Leelawadee UI", 22F, System.Drawing.FontStyle.Bold);
+            this.lblBestPace.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblBestPace.Location = new System.Drawing.Point(301, 256);
+            this.lblBestPace.Name = "lblBestPace";
+            this.lblBestPace.Size = new System.Drawing.Size(35, 41);
+            this.lblBestPace.TabIndex = 13;
+            this.lblBestPace.Text = "0";
+            // 
+            // lblTotalAvgPace
+            // 
+            this.lblTotalAvgPace.AutoSize = true;
+            this.lblTotalAvgPace.Font = new System.Drawing.Font("Leelawadee UI", 22F, System.Drawing.FontStyle.Bold);
+            this.lblTotalAvgPace.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblTotalAvgPace.Location = new System.Drawing.Point(301, 186);
+            this.lblTotalAvgPace.Name = "lblTotalAvgPace";
+            this.lblTotalAvgPace.Size = new System.Drawing.Size(35, 41);
+            this.lblTotalAvgPace.TabIndex = 10;
+            this.lblTotalAvgPace.Text = "0";
+            // 
+            // lblTotalRuns
+            // 
+            this.lblTotalRuns.AutoSize = true;
+            this.lblTotalRuns.Font = new System.Drawing.Font("Leelawadee UI", 22F, System.Drawing.FontStyle.Bold);
+            this.lblTotalRuns.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblTotalRuns.Location = new System.Drawing.Point(301, 46);
+            this.lblTotalRuns.Name = "lblTotalRuns";
+            this.lblTotalRuns.Size = new System.Drawing.Size(35, 41);
+            this.lblTotalRuns.TabIndex = 9;
+            this.lblTotalRuns.Text = "0";
+            // 
+            // lblTotalDistance
+            // 
+            this.lblTotalDistance.AutoSize = true;
+            this.lblTotalDistance.Font = new System.Drawing.Font("Leelawadee UI", 22F, System.Drawing.FontStyle.Bold);
+            this.lblTotalDistance.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblTotalDistance.Location = new System.Drawing.Point(301, 116);
+            this.lblTotalDistance.Name = "lblTotalDistance";
+            this.lblTotalDistance.Size = new System.Drawing.Size(35, 41);
+            this.lblTotalDistance.TabIndex = 8;
+            this.lblTotalDistance.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(58, 131);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(144, 21);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Total Distance (KM)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(58, 61);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(82, 21);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Total Runs";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(58, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 21);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Best Pace (min/km)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Overall Avg Pace (min/km)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(513, 834);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotalDistance;
+        private System.Windows.Forms.Label lblTotalRuns;
+        private System.Windows.Forms.Label lblTotalAvgPace;
+        private System.Windows.Forms.Label lblBestPace;
+    }
+}
+
---
+++ Run tracker app/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Run_tracker_app
+{
+    public partial class Form1 : Form
+    {
+        private List<Run> runs = new List<Run>();
+        private string filePath = "data.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView.View = View.Details;
+            listView.Columns.Add("Date", 94);
+            listView.Columns.Add("Distance (KM)", 95);
+            listView.Columns.Add("Time (MM:SS)", 95);
+            listView.Columns.Add("Pace (min/km)", 95);
+            listView.Columns.Add("Avg Speed (Kph)", 95);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Input Validation
+            bool validDistance = double.TryParse(txtDistance.Text, out double distance);
+            bool validTime = TimeSpan.TryParseExact(txtTime.Text, "mm\\:ss", null, out TimeSpan time);
+
+            if (!validDistance || distance < 0 || distance >= 100)
+            {
+                MessageBox.Show("Please enter a valid distance in the format (00.00)");
+            }
+            else if (!validTime)
+            {
+                MessageBox.Show("Please enter a valid time in the format (MM:SS)");
+            }
+            else
+            {
+                // Objects and add Runs
+                Run runObject = new Run();
+                runObject.date = datePicker.Value;
+                runObject.distance = double.Parse(txtDistance.Text);
+                runObject.time = time;
+
+                double pace = time.TotalMinutes / distance;
+                double avgspeed = distance / time.TotalHours;
+
+                runObject.pace = pace;
+                runObject.avgSpeed = avgspeed;
+
+                runs.Add(runObject);
+                AddToListView(runObject);
+
+                // Clear Fields
+                datePicker.Value = DateTime.Now;
+                txtDistance.Text = "";
+                txtTime.Text = "";
+
+                // Confirmation MB
+                MessageBox.Show("Run Successfully Logged");
+
+                // Update overall stats and save data
+                UpdateOverallStats();
+                SaveData();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveData();
+        }
+
+        private void LoadData()
+        {
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(';');
+                    if (parts.Length == 5)
+                    {
+                        DateTime date = DateTime.Parse(parts[0]);
+                        double distance = double.Parse(parts[1]);
+                        TimeSpan time = TimeSpan.Parse(parts[2]);
+                        double pace = double.Parse(parts[3]);
+                        double avgSpeed = double.Parse(parts[4]);
+
+                        Run runObject = new Run
+                        {
+                            date = date,
+                            distance = distance,
+                            time = time,
+                            pace = pace,
+                            avgSpeed = avgSpeed
+                        };
+
+                        runs.Add(runObject);
+                        AddToListView(runObject);
+                    }
+                }
+
+                UpdateOverallStats();
+            }
+        }
+
+        private void SaveData()
+        {
+            List<string> lines = new List<string>();
+            foreach (Run run in runs)
+            {
+                string line = $"{run.date};{run.distance};{run.time};{run.pace};{run.avgSpeed}";
+                lines.Add(line);
+            }
+            File.WriteAllLines(filePath, lines);
+        }
+
+        private void AddToListView(Run run)
+        {
+            ListViewItem listItem = new ListViewItem(run.date.ToShortDateString());
+            listItem.SubItems.Add(run.distance.ToString());
+            listItem.SubItems.Add(run.time.ToString("mm\\:ss"));
+            listItem.SubItems.Add(TimeSpan.FromMinutes(run.pace).ToString("mm\\:ss"));
+            listItem.SubItems.Add(run.avgSpeed.ToString("0.00"));
+            listView.Items.Add(listItem);
+        }
+
+        private void UpdateOverallStats()
+        {
+            double totaldistance = runs.Sum(run => run.distance);
+            int totalruns = runs.Count;
+
+            if (totalruns > 0)
+            {
+                double totalavgpace = runs.Sum(run => run.pace);
+                double tavgpace = totalavgpace / totalruns;
+                TimeSpan ttavg = TimeSpan.FromMinutes(tavgpace);
+                lblTotalAvgPace.Text = ttavg.ToString("mm\\:ss");
+
+                double fastestPace = runs.Min(run => run.pace);
+                TimeSpan fastestpacets = TimeSpan.FromMinutes(fastestPace);
+                lblBestPace.Text = fastestpacets.ToString("mm\\:ss");
+            }
+            else
+            {
+                lblTotalAvgPace.Text = "00:00";
+                lblBestPace.Text = "00:00";
+            }
+
+            lblTotalDistance.Text = totaldistance.ToString();
+            lblTotalRuns.Text = totalruns.ToString();
+        }
+
+        public class Run
+        {
+            public DateTime date { get; set; }
+            public double distance { get; set; }
+            public TimeSpan time { get; set; }
+            public double pace { get; set; }
+            public double avgSpeed { get; set; }
+        }
+    }
+}
+
